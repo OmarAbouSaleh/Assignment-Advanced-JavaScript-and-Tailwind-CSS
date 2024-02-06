95f3efe4668b451500c70b4167a1af711a36a74c
louiebadillo <marklouie.badillo@edu.sait.ca>
2024-02-06 16:29:25 -0700

    Added weight and temp conversion functions
    generated conversions for weight and temperature based on joshua's distance conversion function.the corresponding html files were tweeked to work with the id's used for input, select and results. onclick button actions were also added.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+//Distance function
 function distanceConversion() {
   var input = document.getElementById("input").value;
   var select = document.getElementById("select").value;
@@ -22,6 +24,69 @@ function distanceConversion() {
         result.push(answer);
       }
     }
+  }
+
+  resultElement.value = result.join(", ");
+}
+
+
+
+// Temperature function
+function temperatureConversion() {
+  var input = document.getElementById("tempInput").value;
+  var select = document.getElementById("tempSelect").value;
+  var resultElement = document.getElementById("tempResult");
+  var result = [];
+
+  var arr = input.split(",");
+
+  if (select == "Celsius to Fahrenheit") {
+    arr.forEach(value => {
+      var numValue = parseFloat(value);
+      if (!isNaN(numValue)) {
+        var answer = ((numValue * 9/5) + 32).toFixed(2);
+        result.push(answer);
+      }
+    });
+  } else if (select == "Fahrenheit to Celsius") {
+    arr.forEach(value => {
+      var numValue = parseFloat(value);
+      if (!isNaN(numValue)) {
+        var answer = ((numValue - 32) * 5/9).toFixed(2);
+        result.push(answer);
+      }
+    });
+  }
+
+  resultElement.value = result.join(", ");
+}
+
+
+// Weight function
+function weightConversion() {
+  var input = document.getElementById("weightInput").value;
+  var select = document.getElementById("weightSelect").value;
+  var resultElement = document.getElementById("weightResult");
+  var result = [];
+
+  var arr = input.split(",");
+
+  if (select == "Pounds to Kilograms") {
+    arr.forEach(value => {
+      var numValue = parseFloat(value);
+      if (!isNaN(numValue)) {
+        var answer = (numValue * 0.453592).toFixed(2);
+        result.push(answer);
+      }
+    });
+  } else if (select == "Kilograms to Pounds") {
+    arr.forEach(value => {
+      var numValue = parseFloat(value);
+      if (!isNaN(numValue)) {
+        var answer = (numValue / 0.453592).toFixed(2);
+        result.push(answer);
+      }
+    });
   }
 
   resultElement.value = result.join(", ");
